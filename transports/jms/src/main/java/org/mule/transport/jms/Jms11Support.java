b7f3f7236e6fe19d0803dfe1acffa60c9f92fbdd
Pablo Balbi <pablo.l.balbi@gmail.com>
2019-03-14 14:38:40 -0300

    Forcing transacted sessions

--- transports/jms/src/main/java/org/mule/transport/jms/Jms11Support.java
+++ transports/jms/src/main/java/org/mule/transport/jms/Jms11Support.java
@@ -79,6 +79,10 @@ public class Jms11Support implements JmsSupport
                                  int ackMode,
                                  boolean noLocal) throws JMSException
     {
+        if (System.getProperty("mule.jms.alwaysTransacted") != null)
+        {
+            transacted = true;
+        }
         return connection.createSession(transacted, (transacted ? Session.SESSION_TRANSACTED : ackMode));
     }
 
